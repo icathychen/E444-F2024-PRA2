2b8d47fe72e563a7ddb2a812931700c1adc2f974
Cathy Chen <cathycat0131@gmail.com>
2024-09-25 23:02:06 -0400

    Added field for UofT email and validator error

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField 
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, StopValidation, ValidationError
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -15,9 +15,28 @@ moment = Moment(app)
 def user(name):
     return render_template('user.html', name=name, current_time=datetime.utcnow())
 
+class CustomEmail(Email):
+    def __init__(self, message=None):
+        if not message:
+            message = "Invalid email."
+        super(CustomEmail, self).__init__(message=message)
+
+    def __call__(self, form, field):
+        if '@' not in field.data:
+            message = ("Please include an \'@\' in the email address. \'%s\' is missing an \'@\'.") % field.data
+            raise ValidationError(message=message)
+        super(CustomEmail, self).__call__(message=message)
+
+
+def validate_email(form, field):
+        if '@' not in (field.data):
+            field.errors[:] = []
+            message = field.gettext('Please include an \'@\' in the email address. \'%s\' is missing an \'@\'.') % field.data
+            raise ValidationError(message)
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    # email = StringField('What is your UofT email address?', validators=[DataRequired(), Email(message="")])
+    email = StringField('What is your UofT email address?', validators=[DataRequired(), CustomEmail()])
     submit = SubmitField('Submit')
     
 @app.route('/', methods=['GET', 'POST'])
@@ -25,9 +44,13 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!') 
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!') 
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html',form = form, name = session.get('name'))
+    return render_template('index.html',form = form, name = session.get('name'), email=session.get('email'))
 
